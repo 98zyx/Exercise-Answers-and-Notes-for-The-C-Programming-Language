7639a234d89d66c8a1b6e430cd486f66d1e33f22
98zyx <zhuyuanxin98@outlook.com>
2020-12-29 20:20:32 +0800

    update 1.20

--- Chapter1/Exercise/1_20.c
+++ Chapter1/Exercise/1_20.c
@@ -6,7 +6,7 @@ int main()
     int rest = N;
     int c;
     while ((c = getchar()) != EOF)
-    {   
+    {
         if (c == '\t')
         {
             int i;
@@ -18,16 +18,12 @@ int main()
         else
         {
             putchar(c);
-            if (c != '\n')
+            rest--;
+            if (rest < 0 || c == '\n')
             {
-                rest--;
-                if (rest < 0)
-                {
-                    rest = N;
-                }
+                rest = N;
             }
-        }   
+        }
     }
     return 0;
-    
 }
